a10ee751cbada589f2ad15c606a46d6cfd89f656
Jonatas <jonataspereira788@gmail.com>
2023-04-18 13:14:54 -0300

    130420230119

--- ex05.py
+++ ex05.py
@@ -1,1 +1,9 @@
 #Escreva uma função que receba um dicionário como entrada e retorna uma nova lista contendo apenas as chaves
+
+dicionario = {'neto':'estudante','anderson':'professor', 'pc':'computador'}
+
+def ordenar_chaves(dicionario):
+    chaves = dicionario.keys()
+    print(chaves)
+        
+ordenar_chaves(dicionario)
--- ex06.py
+++ ex06.py
@@ -1,1 +1,11 @@
 #Escreva uma função que receba um dicionário como entrada e retorna uma nova lista com as chaves ordenadas em ordem alfabética.
+
+dicionario = {'neto':'estudante','anderson':'professor', 'pc':'computador'}
+
+def ordenar_chaves(dicionario):
+    chaves_ordenadas = sorted(dicionario.keys())
+    lista_ordenada = [(chave, dicionario[chave]) for chave in chaves_ordenadas]
+    print(lista_ordenada)
+        
+
+ordenar_chaves(dicionario)
